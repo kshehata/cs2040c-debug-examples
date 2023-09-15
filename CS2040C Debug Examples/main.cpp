76a013ac7b80e66ef0171dea840293b49c166b9b
Kareem M Shehata <e0546273@nus.edu.sg>
2023-09-15 18:35:19 +0800

    Added a third step for factorials and nicer prints.

--- CS2040C Debug Examples/main.cpp
+++ CS2040C Debug Examples/main.cpp
@@ -16,7 +16,8 @@ int factorial(int n) {
 int main() {
 	LinkedList<int>* list = new LinkedList<int>();
 
-	// Step 1: Fix LinkedList so the output below is correct.
+	cout << "Step 1" << endl;
+	// Fix LinkedList so the output below is correct.
 	list->insertHead(9)
 		->insertHead(17)
 		->insertHead(13)
@@ -25,8 +26,15 @@ int main() {
 	cout << "Should be (5): -1, 42, 13, 17, 9, " << endl << "     ";
 	list->print();
 
-	// Step 2: Fix LinkedList so the output below is correct.
+	cout << endl << "Step 2" << endl;
+	// Fix LinkedList so the output below is correct.
 	list->remove(42)->remove(555)->remove(9)->remove(-1);
 	cout << "Should be (2): 13, 17, " << endl << "     ";
 	list->print();
+
+	cout << endl << "Step 3" << endl;
+	// Fix the output below
+	for (int i = 10; i < 16; ++i) {
+		cout << i << "! = " << factorial(i) << endl;
+	}
 }
